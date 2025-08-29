563bdf4fbd8812ed01f1e33c4c1021a2f401d2e0
MagicKery <142007347+MagicKery@users.noreply.github.com>
2025-08-29 23:44:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let gift = document.getElementById("donate");
 function donate(){
     Swal.fire({
-        title: "HappyBirthday!",
+        title: "Thank you very much for stay here!",
         html:  `
         <div>
             <img src="images/qr.jpg" style="border-radius: 15%; height: 30rem; margin: auto; padding: 2rem;">
         </div>
         <div>
-            Thank you very much for stay here!!
+            I'm very happy if you are here. I'll never forget you.
         </div>
         `,
         text: "",
         });
+
     }
