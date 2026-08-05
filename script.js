6cf20dcdeb3799b44d261258a666eebf12f702ca
MagicKery <142007347+MagicKery@users.noreply.github.com>
2026-08-05 13:41:54 +0700

    Add image download functionality with confirmation

---
+++ script.js
@@ -0,0 +1,32 @@
+function downloadImage(url, filename) {
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = filename || url.split('/').pop();
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+
+        let donateBtn = document.querySelector('.btn-donate');
+        donateBtn.addEventListener('click', function(e) {
+            e.preventDefault();
+            Swal.fire({
+            title: "Do you want to save this image?",
+            imageUrl: "abaQR.webp",
+            imageWidth: 200,
+            showDenyButton: true,
+            confirmButtonText: "Download",
+            denyButtonText: `Cancel`
+            }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                downloadImage('abaQR.webp', 'abaQR.webp');
+                Swal.fire(
+                    "Downloaded!",
+                    "The image download should begin automatically.",
+                    "success"
+                );
+            }
+            else if (result.isDenied) Swal.fire("There might be an issue", "", "info");
+            });
+        });
